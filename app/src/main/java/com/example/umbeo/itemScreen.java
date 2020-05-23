16d68dfa9e42ca51df16533e7f274a35cc24f7f1
mukulsh98 <sharma.mukul938@gmail.com>
2020-05-23 20:31:26 +0530

    updated first activity to be displayed.

--- app/src/main/java/com/example/umbeo/itemScreen.java
+++ app/src/main/java/com/example/umbeo/itemScreen.java
@@ -1,14 +1,27 @@
 package com.example.umbeo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class itemScreen extends AppCompatActivity {
 
+    Button buy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_screen);
+
+
+        buy=(Button)findViewById(R.id.buynow);
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),login.class));
+            }
+        });
     }
 }
