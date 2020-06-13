109795235e298eed847a035da8a20b7a895e04cf
meghagunjanmg <meghagunjanmg@gmail.com>
2020-06-13 15:28:38 +0530

    UI dashbord,signin,signup improvement

--- app/src/main/java/com/example/umbeo/dashboard.java
+++ app/src/main/java/com/example/umbeo/dashboard.java
@@ -1,12 +1,9 @@
 package com.example.umbeo;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class dashboard extends AppCompatActivity {
 
@@ -15,7 +12,6 @@ public class dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-
 
     }
 }
--- app/src/main/java/com/example/umbeo/homescreen.java
+++ app/src/main/java/com/example/umbeo/homescreen.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
@@ -12,13 +13,11 @@ import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 public class homescreen extends Fragment {
 
     TextView log,fruit;
-    ImageView address;
+    TextView address;
     CardView fruits;
     @Nullable
     @Override
@@ -40,7 +39,7 @@ public class homescreen extends Fragment {
                 startActivity(new Intent(getActivity(),login.class));
             }
         });
-        address=(ImageView)v.findViewById(R.id.addre);
+        address=(TextView) v.findViewById(R.id.addre);
         address.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
